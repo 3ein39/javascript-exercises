317b1a47ac2fdcc6c8ede3e0b5cd006d886d245e
HusseinHany <hhany3997@gmail.com>
2023-03-17 22:28:30 +0200

    Solve p2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(str, n) {
+    let ans = "";
 
+    if (n < 0)
+        return "ERROR";
+
+    for (let i = 0; i < n; ++i) {
+        ans += str;
+    }
+
+    return ans;
 };
 
 // Do not edit below this line
