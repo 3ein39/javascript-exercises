2b07c11b6e347b9c0103cedabd88da9f2e2da345
HusseinHany <hhany3997@gmail.com>
2023-03-18 21:11:43 +0200

    Solve p5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,20 @@
-const sumAll = function() {
+const sumAll = function(l, r) {
+    if (r < l) {
+        let temp = l;
+        l = r;
+        r = temp;
+    }
 
+    if (l < 0 || r < 0 || typeof l !== "number" || typeof r !== "number")
+        return 'ERROR';
+
+    let sum = 0;
+    for (let i = l; i <= r; ++i) {
+        sum += i;
+    }
+
+    return sum;
 };
-
+// console.log(typeof 5);
 // Do not edit below this line
 module.exports = sumAll;
