5c1e73fc2a482b9982ac47876fec9061689db460
HusseinHany <hhany3997@gmail.com>
2023-03-19 12:37:27 +0200

    Solve p6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,9 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    return year % 4 === 0 && ( (year % 100 !== 0 ) || (year % 400 === 0) );
 };
+
+// let ans = leapYears(1900);
+// console.log(1900 % 400);
 
 // Do not edit below this line
 module.exports = leapYears;
